7b26ef98df2fd3f2d5014b5c6feab6e5670d9c7b
Wendell Gomes <wendell_gomes@id.uff.br>
2020-01-14 14:38:29 -0300

    Aula 229 - CRUD Scaffolding

---
+++ Data/TesteContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Teste.Models
+{
+    public class TesteContext : DbContext
+    {
+        public TesteContext (DbContextOptions<TesteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Teste.Models.Department> Department { get; set; }
+    }
+}
