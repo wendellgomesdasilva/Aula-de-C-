b33b798d6da60bd619ab4b2c606914b20a6d66ad
Wendell Gomes <wendell_gomes@id.uff.br>
2020-01-16 12:32:51 -0300

    Aula 232 - Other entities and second migration

--- Data/TesteContext.cs
+++ Data/TesteContext.cs
@@ -13,6 +13,8 @@ namespace Teste.Models
         {
         }
 
-        public DbSet<Teste.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ Models/Enums/SalesStatus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Teste.Models.Enums
+{
+    public enum SalesStatus : int
+    {
+        Peding = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
