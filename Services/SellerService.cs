9e6059db6df04c449cfb993479fcfbfef194f4dc
Wendell Gomes <wendell_gomes@id.uff.br>
2020-01-17 13:39:44 -0300

    Aula 240 - Seller details and eager loading

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Teste.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Teste.Services
 {
@@ -28,7 +29,7 @@ namespace Teste.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
