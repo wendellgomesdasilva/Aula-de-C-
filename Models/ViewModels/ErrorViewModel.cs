9308ac07a6f9f1b7d97eea7eba2533d23b781f9b
Wendell Gomes <wendell_gomes@id.uff.br>
2020-01-17 14:50:19 -0300

    Aula 242 - Returning custom error page

--- Models/ViewModels/ErrorViewModel.cs
+++ Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace Teste.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
