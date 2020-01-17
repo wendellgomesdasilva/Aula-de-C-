86154b955f66f7c0d1c08af519a1e568f0ad01f2
Wendell Gomes <wendell_gomes@id.uff.br>
2020-01-17 12:38:46 -0300

    Aula 238 - SellerFormViewModel and Department select component

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Teste.Models;
+using Teste.Models.ViewModels;
 using Teste.Services;
 
 namespace Teste.Controllers
@@ -11,10 +12,12 @@ namespace Teste.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService) 
+        public SellersController(SellerService sellerService, DepartmentService departmentService) 
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -25,7 +28,9 @@ namespace Teste.Controllers
 
         public IActionResult Create() 
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ Services/DepartmentService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Teste.Models;
+
+namespace Teste.Services
+{
+    public class DepartmentService
+    {
+        private readonly TesteContext _context;
+
+        public DepartmentService(TesteContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
