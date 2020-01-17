76ce1c2948b38fe79967d1d1212d5a741ed657a2
Wendell Gomes <wendell_gomes@id.uff.br>
2020-01-17 14:33:59 -0300

    Aula 241 - Update seller and custom service exception

---
+++ Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Teste.Services.Exceptions
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+        
+    }
+}
