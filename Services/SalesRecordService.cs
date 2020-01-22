ff659f12cc7604b77e3872ec6a7a010c0ddcc7e6
Wendell Gomes <wendell_gomes@id.uff.br>
2020-01-22 14:01:08 -0300

    Aula 250 - Implementing simple search

---
+++ Services/SalesRecordService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Teste.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Teste.Services
+{
+    public class SalesRecordService
+    {
+        private readonly TesteContext _context;
+
+        public SalesRecordService(TesteContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x=> x.Date)
+                .ToListAsync();
+        }
+    }
+}
